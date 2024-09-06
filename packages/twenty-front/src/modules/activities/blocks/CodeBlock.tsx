a1da204e6afd5d6c3e58341a93830f4817fdb825
Faisal-imtiyaz123 <ee23btech11019@iith.ac.in>
2024-09-07 01:06:55 +0530

    creates CodeBlock block

---
+++ packages/twenty-front/src/modules/activities/blocks/CodeBlock.tsx
@@ -0,0 +1,26 @@
+import { CodeEditor } from '@/ui/input/code-editor/components/CodeEditor';
+import { createReactBlockSpec } from '@blocknote/react';
+
+export const CodeBlock = createReactBlockSpec(
+  {
+    type: 'codeBlock',
+    propSchema: {
+      code: {
+        default: '',
+      },
+    },
+    content: 'none',
+  },
+  {
+    render: ({ block}) => {
+      return (
+        <CodeEditor
+          value={block.props.code}
+          focusOnMount={true}
+          height={200}
+          header={<></>}
+        />
+      );
+    },
+  },
+);
